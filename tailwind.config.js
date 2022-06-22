476522e20f2740f090ac6ddf0398bc858afa8757
ponbac <root@ponbac.xyz>
2022-06-22 18:51:53 +0200

    darker text color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#FFCC8F",
         primaryLight: "#FFDAAF",
-        secondary: "#53179c",
+        secondary: "#6a21c2",
         secondaryLight: "#A760FF",
       },
       fontFamily: {
         novaMono: ["Nova Mono", "sans-serif"],
-      }
+      },
     },
   },
   plugins: [],
-}
+};
